0bec7b958ff051d26ec199711040bb2d3647b2ce
Samy CHOUAM <sachouam@e1r12p19.42.fr>
2022-02-05 18:16:42 +0100

    stack.hpp prototyped

--- stack.hpp
+++ stack.hpp
@@ -5,10 +5,63 @@
 
 namespace ft
 {
-	template <class T, class Container = deque<T>>
-	class stack
+	template <class T, class Container = std::deque<T> >
+	class stack 
 	{
-		stack<T, Container>();
-		~stack<T, Container>();
+		protected:
+		
+		Container c;
+
+		public:
+	
+		typedef typename Container::value_type value_type;
+		typedef typename Container::size_type size_type;
+		typedef Container container_type;
+
+		explicit stack(const Container& = Container());
+		
+		bool empty() const 
+		{ 
+			return c.empty(); 
+		}
+		size_type size() const 
+		{ 
+			return c.size();
+		}
+		value_type& top() 
+		{ 
+			return c.back(); 
+		}
+		const value_type& top() const
+		{ 
+			return c.back(); 
+		}
+		void push(const value_type& x)
+		{ 
+			c.push_back(x); 
+		}
+		void pop() 
+		{ 
+			c.pop_back(); 
+		}
 	};
+
+	template <class T, class Container>
+	bool operator==(const stack<T, Container>& x,
+	const stack<T, Container>& y);
+	template <class T, class Container>
+	bool operator< (const stack<T, Container>& x,
+	const stack<T, Container>& y);
+	template <class T, class Container>
+	bool operator!=(const stack<T, Container>& x,
+	const stack<T, Container>& y);
+	template <class T, class Container>
+	bool operator> (const stack<T, Container>& x,
+	const stack<T, Container>& y);
+	template <class T, class Container>
+	bool operator>=(const stack<T, Container>& x,
+	const stack<T, Container>& y);
+	template <class T, class Container>
+	bool operator<=(const stack<T, Container>& x,
+	const stack<T, Container>& y);
 };
