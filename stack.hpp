7b62314f4ad2325db0c248659b0ebe2832558531
Samy CHOUAM <sachouam@e1r12p19.42.fr>
2022-02-05 20:55:18 +0100

    stack.hpp : debut operation overloads non-members

--- stack.hpp
+++ stack.hpp
@@ -10,58 +10,92 @@ namespace ft
 	{
 		protected:
 		
-		Container c;
+			Container c;
 
 		public:
 	
-		typedef typename Container::value_type value_type;
-		typedef typename Container::size_type size_type;
-		typedef Container container_type;
+			typedef typename Container::value_type 		value_type;
+			typedef typename Container::size_type 		size_type;
+			typedef Container 							container_type;
 
-		explicit stack(const Container& = Container());
+			explicit stack(const container_type& ctnr = container_type())
+			{
+				
+			}
 
-		bool empty() const 
-		{ 
-			return c.empty(); 
-		}
-		size_type size() const 
-		{ 
-			return c.size();
-		}
-		value_type& top() 
-		{ 
-			return c.back(); 
-		}
-		const value_type& top() const
-		{ 
-			return c.back(); 
-		}
-		void push(const value_type& x)
-		{ 
-			c.push_back(x); 
-		}
-		void pop() 
-		{ 
-			c.pop_back(); 
-		}
+			bool empty() const 
+			{ 
+				return c.empty(); 
+			}
+			size_type size() const 
+			{ 
+				return c.size();
+			}
+			value_type& top() 
+			{ 
+				return c.back(); 
+			}
+			const value_type& top() const
+			{ 
+				return c.back(); 
+			}
+			void push(const value_type& x)
+			{ 
+				c.push_back(x); 
+			}
+			void pop() 
+			{ 
+				c.pop_back(); 
+			}
+				friend bool operator==(const stack<T, Container>& x, const stack<T, Container>& y);
+				friend bool operator< (const stack<T, Container>& x, const stack<T, Container>& y);
+				friend bool operator!=(const stack<T, Container>& x, const stack<T, Container>& y);
+				friend bool operator> (const stack<T, Container>& x, const stack<T, Container>& y);
+				friend bool operator>=(const stack<T, Container>& x, const stack<T, Container>& y);
+				friend bool operator<=(const stack<T, Container>& x, const stack<T, Container>& y);
 	};
 
 	template <class T, class Container>
-	bool operator==(const stack<T, Container>& x,
-	const stack<T, Container>& y);
+		bool operator==(const stack<T, Container>& x, const stack<T, Container>& y)
+		{
+			typename std::stack<T>::container_type::const_iterator itx = x.c.begin();
+			typename std::stack<T>::container_type::const_iterator ity = y.c.begin();
+			typename std::stack<T>::container_type::const_iterator itxe = x.c.end();
+			typename std::stack<T>::container_type::const_iterator itye = y.c.end();
+
+			while (itx != itxe && ity != itye)
+			{
+				if (*itx != *ity)
+					return (false);
+				itx++;
+				ity++;
+			}
+			return (true);
+		}
+	
 	template <class T, class Container>
-	bool operator< (const stack<T, Container>& x,
-	const stack<T, Container>& y);
+		bool operator< (const stack<T, Container>& x, const stack<T, Container>& y)
+		{
+			
+		}
 	template <class T, class Container>
-	bool operator!=(const stack<T, Container>& x,
-	const stack<T, Container>& y);
+		bool operator!=(const stack<T, Container>& x, const stack<T, Container>& y)
+		{
+			
+		}
 	template <class T, class Container>
-	bool operator> (const stack<T, Container>& x,
-	const stack<T, Container>& y);
+		bool operator> (const stack<T, Container>& x, const stack<T, Container>& y)
+		{
+			
+		}
 	template <class T, class Container>
-	bool operator>=(const stack<T, Container>& x,
-	const stack<T, Container>& y);
+		bool operator>=(const stack<T, Container>& x, const stack<T, Container>& y)
+		{
+			
+		}
 	template <class T, class Container>
-	bool operator<=(const stack<T, Container>& x,
-	const stack<T, Container>& y);
+		bool operator<=(const stack<T, Container>& x, const stack<T, Container>& y)
+		{
+			
+		}
 };
--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include "stack.hpp"
 
 int		main()
 {
 	std::vector<int> test;
+	ft::stack<char> test2;
+	ft::stack<char> test3;
 
 	for (int i = 0; i < 20; i++)
 	{
@@ -15,6 +19,26 @@ int		main()
 		std::cout << "capacity : " << test.capacity() << std::endl;
 		std::cout << i << std::endl << std::endl;
 	}
-
 	std::cout << "max_size : " << test.max_size() << std::endl;
+
+	test2.push('z');
+	test2.push('a');
+	test3.push('a');
+	test3.push('a');
+
+	bool te;
+	te = (test2 == test3);
+	std::cout << "test2 == test3 = " << te << std::endl;
+	/*
+	te = (test2 != test3);
+	std::cout << "test2 != test3 = " << te << std::endl;
+	te = (test2 < test3);
+	std::cout << "test2 < test3 = " << te << std::endl;
+	te = (test2 > test3);
+	std::cout << "test2 > test3 = " << te << std::endl;
+	te = (test2 <= test3);
+	std::cout << "test2 <= test3 = " << te << std::endl;
+	te = (test2 >= test3);
+	std::cout << "test2 >= test3 = " << te << std::endl;
+	*/
 }
