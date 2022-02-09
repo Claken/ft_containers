db171b1c85ae75affc3fd96d7206740eadb5ea2b
Claken <samychouam@gmail.com>
2022-02-09 03:16:27 +0100

    utils.hpp : equal et lexico implemented

--- utils.hpp
+++ utils.hpp
@@ -16,10 +16,81 @@ namespace ft
 	{
 
 	};
-	
-	template <class T1, class T2>
-	pair<T1,T2>;
+
+	template <class InputIterator1, class InputIterator2>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+	{
+		while (first1!=last1)
+		{
+			if (!(*first1 == *first2))
+				return false;
+			++first1;
+			++first2;
+		}
+		return true;
+	}
+
+	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+	{
+		while (first1!=last1)
+		{
+			if (!(pred(*first1, *first2)))
+				return false;
+			++first1;
+			++first2;
+		}
+		return true;
+	}
+
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1!=last1)
+		{
+			if (first2==last2 || *first2 < *first1)
+			{
+				return false;
+			}
+			else if (*first1 < *first2)
+			{
+				return true;
+			}
+			++first1;
+			++first2;
+		}
+		return (first2!=last2);
+	}
+
+	template <class InputIterator1, class InputIterator2, class Compare>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
+	{
+		while (first1!=last1)
+		{
+			if (first2==last2 || comp(*first2, *first1))
+			{
+				return false;
+			}
+			else if (comp(*first1, *first2))
+			{
+				return true;
+			}
+			++first1;
+			++first2;
+		}
+		return (first2!=last2);
+	}
+
 
 	template <class T1, class T2>
-	make_pair (T1 x, T2 y);
+	struct pair
+	{
+
+	};
+
+	template <class T1, class T2>
+	pair<T1,T2>		make_pair(T1 x, T2 y)
+	{
+		return ( pair<T1,T2>(x,y) );
+	}
 };
