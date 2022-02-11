f16290865901b4a306720bad70bf6264365b1203
Claken <samychouam@gmail.com>
2022-02-11 13:50:19 +0100

    utils.hpp : std::pair début

--- utils.hpp
+++ utils.hpp
@@ -125,7 +125,30 @@ namespace ft
 	template <class T1, class T2>
 	struct pair
 	{
+		typedef T1 first_type;
+		typedef T2 second_type;
+		
+		public:
 
+		first_type		first;
+		second_type		second;
+		pair()
+		{
+
+		}
+		template<class U, class V>
+		pair (const pair<U,V>& pr)
+		{
+
+		}
+		pair (const first_type& a, const second_type& b)
+		{
+
+		}
+		pair& operator= (const pair& pr)
+		{
+
+		}
 	};
 
 	template <class T1, class T2>
