a444bf057d606333afcebf20c9c11bc71bddf301
Claken <samychouam@gmail.com>
2022-03-12 03:25:19 +0100

    btree : print2D

--- btree.hpp
+++ btree.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include "utils.hpp"
 
+#define SPACE 10
+
 namespace ft
 {
 
@@ -46,13 +48,11 @@ namespace ft
 	template <class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<ft::pair<const Key, T> >, class Allocator2 = std::allocator<Node<Key, T> > >
 	class Tree
 	{
-		private:
-		
-			Node<Key, T>*			_tree;
-			Allocator				_allocator_type;
-			Allocator2				_allocator_node;
 
 		public:
+		
+			typedef ft::pair<const Key, T> 						value_type;
+			typedef Node<Key, T>*								node;
 
 			Tree(const Allocator& = Allocator(), const Allocator2& = Allocator2())
 			{
@@ -85,6 +85,48 @@ namespace ft
 			{
 				return (this->_tree->pair.second);
 			}
+
+			// void insert(const value_type& x)
+			// {
+
+			// }
+
+			node tree()
+			{
+				return (this->_tree);
+			}
+
+			void print2D(node r, int space)
+			{
+				if (r == NULL) // Base case  1
+					return;
+				space += SPACE; // Increase distance between levels   2
+				print2D(r->right, space); // Process right child first 3 
+				std::cout << std::endl;
+				for (int i = SPACE; i < space; i++) // 5 
+					std::cout << " "; // 5.1  
+				std::cout << r->pair.first << "\n"; // 6
+				std::cout << r->pair.second << "\n"; // 6
+				print2D(r->left, space); // Process left child  7
+			}
+
+//   void printPreorder(TreeNode * r) //(current node, Left, Right) 
+//   {
+//     if (r == NULL)
+//       return;
+//     /* first print data of node */
+//     cout << r -> value << " ";
+//     /* then recur on left sutree */
+//     printPreorder(r -> left);
+//     /* now recur on right subtree */
+//     printPreorder(r -> right);
+//   }
+		
+		private:
+		
+			node				_tree;
+			Allocator			_allocator_type;
+			Allocator2			_allocator_node;
 
 	};
 }
--- tests_map/btree_test.cpp
+++ tests_map/btree_test.cpp
@@ -2,13 +2,14 @@
 
 int main(void)
 {
-    // ft::Tree<std::string, std::string> btree("clé", "test");
-    ft::Tree<std::string, int> btree;
+    ft::Tree<char, std::string> btree('a', "test");
+    // ft::Tree<std::string, int> btree;
 
 
     std::cout << "first test =  " << btree.first() << std::endl;
     std::cout << "second test = " << btree.second() << std::endl;
 
+    btree.print2D(btree.tree(), 0);
 
 
     return (0);
